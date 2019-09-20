df00a8d9a7a89d7e4fcbfb7f6f3dfef50e728bba
ExalFabu <53974096+ExalFabu@users.noreply.github.com>
2019-09-20 14:27:30 +0200

    Used secrets.py constant on authenticate()

--- pyunipolsai/__init__.py
+++ pyunipolsai/__init__.py
@@ -1,8 +1,8 @@
 from .unipolsai import Unipolsai
-from .utils import PositionData
+from .secrets import CREDS, HEADERS
 
 
-def authenticate(credentials: dict, headers: dict) -> Unipolsai:
+def authenticate(credentials: dict=CREDS, headers: dict=HEADERS) -> Unipolsai:
     if credentials.get("username") and credentials.get("password"):
         uni = Unipolsai(credentials, headers)
         uni.authenticate()

3cbf9f1015ac88ab6a74c6a7812a72a8d312445e
ExalFabu <53974096+ExalFabu@users.noreply.github.com>
2019-09-20 19:07:04 +0200

    Added an integrity check for secrets.py

--- pyunipolsai/__init__.py
+++ pyunipolsai/__init__.py
@@ -1,10 +1,39 @@
-from .unipolsai import Unipolsai
-from .secrets import CREDS, HEADERS
+"""
+Questo script ti permette di trovare la posizione della tua auto,
+assicurata con UnipolSai compresa di Unibox (localizzatore GPS)
+
+Guarda la documentazione per vedere come iniziare ad usare questa libreria
+https://github.com/ExalFabu/pyunipolsai
+"""
+from pyunipolsai.unipolsai import Unipolsai
+from pyunipolsai.secrets import CREDS, HEADERS
+from pyunipolsai.utils import PositionData
+from pyunipolsai.urls import UNIPOLSAI_BASE, API_URL, HOME_URL, LOGIN_URL, POST_LOGIN
 
 
-def authenticate(credentials: dict=CREDS, headers: dict=HEADERS) -> Unipolsai:
-    if credentials.get("username") and credentials.get("password"):
+def check_secrets_integrity() -> bool:
+    """
+    Checks if every variable in secrets.py
+    :return: True if every variable in secrets.py is set
+    :raises: AttributeError if one variable isn't set
+    TODO: at least check if they aren't the sample value I put in the template, at least
+    """
+    if (CREDS.get("username") and CREDS.get("password") and HEADERS.get('api_key') and
+            HEADERS.get('x-ibm-client-id') and HEADERS.get('x-ibm-client-secret') and HEADERS.get('company_id') and
+            HEADERS.get('service_type')):
+        return True
+    raise AttributeError("INSERT ERROR ABOUT BAD CREDENTIALS ATTRIBUTE")
+
+
+def authenticate(credentials: dict = CREDS, headers: dict = HEADERS) -> Unipolsai:
+    """
+    This method creates an instance of the class Unipolsai with the credentials that are already on secrets.py
+    :param credentials: dict with username and password
+    :param headers: headers needed to authorize the operations
+    :return: Unipolsai API object already authenticated
+    :type: Unipolsai
+    """
+    if check_secrets_integrity():
         uni = Unipolsai(credentials, headers)
         uni.authenticate()
         return uni
-    raise AttributeError("INSERT ERROR ABOUT BAD CREDENTIALS ATTRIBUTE")
--- pyunipolsai/__main__.py
+++ pyunipolsai/__main__.py
@@ -3,18 +3,21 @@
 import argparse
 import pyunipolsai
 PLATE = pyunipolsai.secrets.PLATE
+required_plate = not PLATE
 
-parser = argparse.ArgumentParser(description=("Questo script ti permette di trovare la posizione della tua auto,"
+parser = argparse.ArgumentParser(description=("Questa libreria ti permette di trovare la posizione della tua auto,"
                                               "assicurata con UnipolSai compresa di Unibox (localizzatore GPS)"))
 parser.add_argument("-u", "--update", action="store_true", help="Se vero, viene mandata la richiesta di aggiornare la "
                                                                 "posizione.")
 
-parser.add_argument("-p", "--plate", metavar="AA000AA", type=str, default=PLATE,
+parser.add_argument("-p", "--plate", metavar="AA000AA", type=str, default=PLATE, required=required_plate,
                     help="Se inserita, viene utilizzata questa targa "
                          "invece di quella specificata in 'secrets.py' ({})".format(PLATE))
 args = parser.parse_args()
 
 PLATE = args.plate
+pyunipolsai.check_secrets_integrity()
+
 uni = pyunipolsai.authenticate()
 print(uni.is_authenticated)
 position = uni.get_position(plate=PLATE, update=args.update)

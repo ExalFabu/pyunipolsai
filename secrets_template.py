3d7a33046471ba5999ac0be5d9233e0f0198ceb2
ExalFabu <53974096+ExalFabu@users.noreply.github.com>
2019-09-20 14:27:01 +0200

    Added required headers

--- secrets_template.py
+++ secrets_template.py
@@ -1,7 +1,9 @@
-CREDS={ 'username': 'your@mail.com',
-        'password': 'Y0urP4ssW0rd'}
-HEADERS={'api_key': "your_api_key",
-         'x-ibm-client-id': "your_client_id",
-         'x-ibm-client-secret': "your_client_secret"
-         }
-PLATE="your_plate"
+CREDS = {'username': 'your@mail.com',
+         'password': 'Y0urP4ssW0rd'}
+HEADERS = {'api_key': "your_api_key",
+           'x-ibm-client-id': "your_client_id",
+           'x-ibm-client-secret': "your_client_secret",
+           'company_id': "unipolsai",
+           'service_type': "Vehicle"
+           }
+PLATE = "your_plate"
